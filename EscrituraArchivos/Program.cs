631fa6bd135de56fceadea5a3aafd4829b781beb
Olaf Huerta <l21211966@tectijuana.edu.mx>
2022-05-31 13:19:46 -0700

    EscrituraArchivosPeliculas
    ea

--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -9,28 +9,43 @@ namespace EscrituraArchivos
 {
     class Program
     {
+        class Peliculas
+        {
+            //campos
+            public string pelicula, director;
+            public int año;
+
+            //constructor
+            public Peliculas(string pelicula, string director, int año)
+            {
+                this.pelicula = pelicula;
+                this.director = director;
+                this.año = año;
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.Write("Ingrese un nombre: ");
-            string nombre = Console.ReadLine();
+            Console.Write("Ingrese una película: ");
+            string pelicula = Console.ReadLine();
+            Console.Write("Ingrese el nombre del director: ");
+            string director = Console.ReadLine();
+            Console.Write("Ingrese el año de estreno: ");
+            int año = int.Parse(Console.ReadLine());
+
+            Peliculas peli = new Peliculas(pelicula, director, año);
 
             StreamWriter sw = new StreamWriter("ejemplo.txt",true);
             // si el archivo no existe, lo creará
             // si ya existe, escribirá en él
             // true es para agregar y no sobreescribir
 
-            string[] lines = {
-                nombre
-            };
-
-            foreach(string line in lines)
-            {
-                sw.WriteLine(line); // escribe en el archivo
-            }
-            sw.Close(); // se debe cerrar el archivo
+            sw.WriteLine(peli.pelicula+"  "+peli.director+"  "+peli.año);
+            sw.Close();
 
             Console.WriteLine("Escribiendo en el archivo...");
             Console.ReadKey();
+
         }
     }
 }
